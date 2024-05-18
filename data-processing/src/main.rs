3c5f53afd9dfefbde805b35a8e9f568265069e86
Fedor Zevako <fyodor.zevako@gmail.com>
2024-05-18 11:05:46 +0000

    implemented

--- data-processing/src/main.rs
+++ data-processing/src/main.rs
@@ -1,3 +1,5 @@
+#[allow(dead_code)]
+
 trait DataPipeline<T> {
     fn insert(&mut self, item: T);
 
@@ -8,29 +10,31 @@ trait DataPipeline<T> {
     fn filter(&mut self, predicate: fn(&T) -> bool);
 }
 
-struct Validator<T>;
+struct Validator<T> {
+    storage: Vec<T>,
+}
 
 impl<T> Validator<T> {
     fn new() -> Self {
-        todo!()
+        Validator { storage: vec![] }
     }
 }
 
 impl<T> DataPipeline<T> for Validator<T> {
     fn insert(&mut self, item: T) {
-        todo!()
+        self.storage.push(item)
     }
 
     fn insert_many(&mut self, items: impl Iterator<Item = T>) {
-        todo!()
+        self.storage.extend(items)
     }
 
     fn num_valid(&self) -> u32 {
-        todo!()
+        self.storage.len() as u32
     }
 
     fn filter(&mut self, predicate: fn(&T) -> bool) {
-        todo!()
+        self.storage.retain(predicate)
     }
 }
 
@@ -41,5 +45,5 @@ fn main() {
     vals.insert(100);
     vals.filter(|&x| x < 50);
 
-    assert_eq!(vals.num_valid(), 2);
+    assert_eq!(vals.num_valid(), 3);
 }
